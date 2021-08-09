f035df7b2f14d1d01e9619221b6da8b1077eeea3
Hossamhatia <63354777+Hossamhatia@users.noreply.github.com>
2021-08-09 13:10:34 +0200

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,7 +16,7 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            'name'=>'peter parker',
+            'name'=>'hossam hassan',
             'email'=>'hossamhatia1@gmail.com',
             'password'=>Hash::make('12345')
         ]);
